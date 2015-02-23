2c8105c074a1d6059f04ee10f8d2d3852972d71b
Andres Calvo <AlephNull0@users.noreply.github.com>
2015-02-23 15:22:43 -0500

    Import GlasswareBasics project from existing source.

---
+++ GlasswareBasics/app/src/main/java/com/ocddevelopers/androidwearables/glasswarebasics/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ocddevelopers.androidwearables.glasswarebasics;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import com.google.android.glass.media.Sounds;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView helloWorld = new TextView(this);
+        helloWorld.setText("Hello World");
+
+        TuggableView tuggableView = new TuggableView(this, helloWorld);
+        tuggableView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                audioManager.playSoundEffect(Sounds.DISALLOWED);
+            }
+        });
+
+        setContentView(tuggableView);
+    }
+
+}
